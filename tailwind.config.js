50352ca82624b0d1a4e5b98e43f8c2ca8c3a48da
joon-jeong <106567734+junwaa-sys@users.noreply.github.com>
2023-05-01 09:44:47 +1200

    Transfer personal project form Dev Academy Github

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './client/**/*.{js,jsx,ts,tsx,css}',
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
