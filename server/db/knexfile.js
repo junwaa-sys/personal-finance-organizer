4c6c09fc98bd12f6dfb245a8cf3d9e2e1788ff0b
joon jeong <juny.jeong@gmail.com>
2023-09-19 20:59:08 +1200

    update productn db to postgres

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -30,22 +30,24 @@ module.exports = {
   },
 
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: join(__dirname, 'dev.sqlite3'),
-    },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    },
-    // client: 'postgresql',
-    // connection: process.env.DATABASE_URL,
+    // client: 'sqlite3',
+    // useNullAsDefault: true,
+    // connection: {
+    //   filename: join(__dirname, 'dev.sqlite3'),
+    // },
     // pool: {
-    //   min: 2,
-    //   max: 10,
+    //   afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
     // },
-    // migrations: {
-    //   tableName: 'knex_migrations',
-    // },
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_URL,
+      port: process.env.DATABASE_PORT,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
   },
 }
